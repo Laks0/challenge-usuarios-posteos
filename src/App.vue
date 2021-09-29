72048e06522996d09d3a1302cb6d4d245735c325
laks0 <laksjoaquin@gmail.com>
2021-09-29 16:59:43 -0300

    formato de página principal

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,20 @@
       color="primary"
       dark
     >
+			<span class="text-h4 hidden-sm-and-down"><b>ADMIN DE USUARIOS</b></span>
+			<v-spacer></v-spacer>
+
+			<v-btn outlined 
+				href="https://github.com/Laks0/challenge-usuarios-posteos/"
+				target="_blank">
+				Proyecto en github</v-btn>
     </v-app-bar>
 
     <v-main class="pink lighten-4">
 			<ListaUsuarios/>
     </v-main>
 
-		<v-footer padless color="primary" class="lighten-1" dark>
+		<v-footer padless color="primary" dark>
 			<v-col class="text-center" cols="12">
 				<span class="text-h6">Challenge - Joaquín Laks</span>
 			</v-col>
