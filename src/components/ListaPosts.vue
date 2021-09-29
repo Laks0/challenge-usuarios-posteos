1410dcdee7cb850a00c0903abd9a73f73344062c
laks0 <laksjoaquin@gmail.com>
2021-09-29 17:44:13 -0300

    funcionalidad correcta de la selección de posts

--- src/components/ListaPosts.vue
+++ src/components/ListaPosts.vue
@@ -10,28 +10,33 @@
 		<v-card>
 			<v-card-title>
 				<span class="text-h4">Posts de {{usuario}}</span>
+
+				<v-spacer/>
+				<v-btn class="error ma-1" @click="cambiarHabilidad(false)">Deshabilitar</v-btn>
+				<v-btn class="success" @click="cambiarHabilidad(true)">Habilitar</v-btn>
 			</v-card-title>
 
 			<v-card-text>
-				<v-card min-width="600" class="ma-2 elevation-5" v-for="post in posts" :key="post.id">
+				<v-card width="400" class="ma-2 elevation-5 d-inline-block" v-for="post in posts" :key="post.id">
+					<v-card-actions>
+						<span v-if="post.habilitado" class="success pa-2 rounded-pill">HABILITADO</span>
+						<span v-if="!post.habilitado" class="error pa-2 rounded-pill">DESHABILITADO</span>
+						<v-spacer/>
+						<v-checkbox
+								v-model="post.seleccionado"
+								color="success"
+								></v-checkbox>
+					</v-card-actions>
 					<v-card-title>
 						<span class="text-h5">{{post.title}}</span>
 					</v-card-title>
 					<v-card-text>
 						{{post.body}}
 					</v-card-text>
-					<v-card-actions>
-						<v-checkbox
-								v-model="post.habilitado"
-								label="habilitado"
-								color="success"
-								></v-checkbox>
-					</v-card-actions>
 				</v-card>
 			</v-card-text>
 
 			<v-card-actions>
-				<v-spacer></v-spacer>
 				<v-btn class="success" @click="activo = false">Volver</v-btn>
 			</v-card-actions>
 		</v-card>
@@ -59,10 +64,20 @@ export default {
 			axios.get("https://jsonplaceholder.typicode.com/posts?userID="+this.id)
 				.then(res => {
 					this.posts = res.data;
-					this.posts.forEach((post) => post.habilitado = true);
+					this.posts.forEach((post) => { 
+						post.habilitado = true;
+					});
 				})
 				.catch(err => console.error(err));
-		}
+		},
+		cambiarHabilidad(valor) {
+			this.posts.forEach(post => {
+				if (post.seleccionado) {
+					post.habilitado = valor;
+					post.seleccionado = false;
+				}
+			});
+		},
 	},
 
 	created() {
